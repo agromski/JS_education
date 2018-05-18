1d00ce950734f4acd878a92c6a927a9788434c2e
DESKTOP-5D3JDJR\a.gromskaya <anna.gromskaya@ukad-group.com>
2018-05-18 20:25:50 +0300

    add toDoList complited

--- toDoList/js/jquery.main.js
+++ toDoList/js/jquery.main.js
@@ -1,40 +1,81 @@
 // page init
 $(function(){
-	get();
-	get();
-	get();
+	toDoList();
 });
 
 
-var toDoList = [{
+var array = [{
 		title: "Выгулять кота",
-		done: "false",
+		done: false,
 	},
 	{
 		title: "Постирать",
-		done: "true",
+		done: true,
 	},
 	{
 		title: "Вынести мусор",
-		done: "true",
+		done: true,
 	},
 	{
 		title: "Попылесосить",
-		done: "false",
+		done: true,
 	},
 	{
 		title: "Убрать тёплые вещи",
-		done: "false",
+		done: true,
 }];
 
+function toDoList() {
 
-function get() {
-	$('.my-list').html('');
-	$(toDoList).each(function (index, item) {
-		var definition = item.title;
-		var li = $('<li>' + definition + '</li>'+'<li>' + definition + '</li>');
-		$('.my-list').append(li);
+	var form = $('.add-form');
+	var input = form.find('input');
+	var listContainer = $('.my-list');
+
+
+	renderList();
+
+	form.on('submit', function(e){
+		e.preventDefault();
+		if (!input.val()) return;
+		addItem(input.val());
+		form[0].reset();
 	});
 
+	$(listContainer).on('click', '.btn-delete', function(e){
+		e.preventDefault();
+		removeItem(this.dataset.index);
+	});
+
+
+
+	function renderList() {
+		var list = '';
+		$(array).each(function (index, item) {
+			var checkbox = (item.done) ? '<input type="checkbox" checked>' : '<input type="checkbox">';
+			list = list + '<li>' + checkbox + item.title + '<a href="#" data-index="' + index + '" class="btn-delete">Delete</a>' + '</li>';
+		});
+		listContainer.html(list);
+	};
+
+	function addItem(value) {
+		var o = {
+			title: value,
+			done: false
+		};
+		array.push(o);
+		renderList();
+	};
+
+	function removeItem(index) {
+		array.splice(index, 1);
+		renderList();
+	}
+
 };
+
+
+
+
+
+
 
